c1c70b1489facd8d2bbe304f73271c24ddfd0be8
Andrew Casillas <casillasa92@tamu.edu>
2019-02-19 23:53:19 -0600

    build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-
+import React from "react";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
